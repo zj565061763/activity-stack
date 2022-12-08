5368a530bb89931a3c97008eba6b2df84b6c5b3f
zj565061763 <565061763@qq.com>
2022-12-09 02:39:21 +0800

    format

--- lib/src/main/java/com/sd/lib/actstack/LibContentProvider.kt
+++ lib/src/main/java/com/sd/lib/actstack/LibContentProvider.kt
@@ -11,21 +11,13 @@ internal class LibContentProvider : ContentProvider() {
         return true
     }
 
-    override fun query(
-        uri: Uri,
-        projectionArg: Array<String>?,
-        selection: String?,
-        selectionArgs: Array<String>?,
-        sortOrder: String?
-    ): Cursor? = null
+    override fun query(uri: Uri, projection: Array<String>?, selection: String?, selectionArgs: Array<String>?, sortOrder: String?): Cursor? = null
 
     override fun getType(uri: Uri): String? = null
 
-    override fun insert(uri: Uri, contentValues: ContentValues?): Uri? = null
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
 
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int = 0
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int = 0
 
-    override fun update(
-        uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?
-    ): Int = 0
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?): Int = 0
 }

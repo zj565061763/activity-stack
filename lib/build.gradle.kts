6db1d0f5f219b959ac16580e0e4840c3d3ce3513
zj565061763 <565061763@qq.com>
2023-06-07 00:40:21 +0800

    1.1.0-beta01

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "activity-stack"
-val libVersionName = "1.0.0"
+val libVersionName = "1.1.0-beta01"
 
 android {
     namespace ="com.sd.lib.activity.stack"
